e5aa934025374bb91068b871b07a66bbcc120126
sdang <sdang6666@gmail.com>
2025-02-15 17:22:26 +0700

    update map 1

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 5f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        MovePlayer();
+    }
+    void MovePlayer()
+    {
+        Vector2 playerInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        rb.linearVelocity = playerInput.normalized * moveSpeed;
+    }
+}
