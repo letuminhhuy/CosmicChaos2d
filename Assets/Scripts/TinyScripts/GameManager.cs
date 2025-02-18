6f9efbe93e5fcade4555fd37e3d1c6253bf3c79f
sdang <sdang6666@gmail.com>
2025-02-19 00:10:13 +0700

    create energy stone and collect

---
+++ Assets/Scripts/TinyScripts/GameManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    private int count = 0;
+    [SerializeField] private TextMeshProUGUI stoneText;
+
+    void Start()
+    {
+        UpdateStone();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void AddStone(int x)
+    {
+        count += x;
+        UpdateStone();
+    }
+    public void UpdateStone()
+    {
+        stoneText.text = count.ToString() + "/7";
+    }
+}
